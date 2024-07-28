35fda21240c3c99ed0df3a59bb3828d4092467e6
nikhilraghuwanshi <nikhilraghuwanshi75@gmail.com>
2024-07-28 11:54:50 +0530

    variables

---
+++ src/Variables/variables.java
@@ -0,0 +1,21 @@
+package Variables;
+
+public class variables {
+    public static void main(String[] args) {
+//        int key = 890;
+        int num;
+        num = 890;
+        System.out.println(num);
+        num = 45;
+        System.out.println(num);
+
+        float myFloat = 5;
+        System.out.println(myFloat);
+
+        boolean isVegetrian = true;
+        System.out.println(isVegetrian);
+
+
+
+    }
+}
